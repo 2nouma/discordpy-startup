6f2ee2bb49b8965f535898e2ebbde560d19c06f0
2nouma <59654285+2nouma@users.noreply.github.com>
2020-03-15 21:28:53 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -13,7 +13,23 @@ async def on_command_error(ctx, error):
     orig_error = getattr(error, "original", error)
     error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
     await ctx.send(error_msg)
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send('pong')
     
+@bot.command()
+async def neko(ctx):
+    await ctx.send('にゃ～ん')
+    
+    bot.run(token)
+    
+import discord
+token = os.environ['DISCORD_BOT_TOKEN']
+# 接続に必要なオブジェクトを生成
+client = discord.Client()    
+
+@client.event
 async def on_raw_reaction_add(payload):
     # channel_id から Channel オブジェクトを取得
     channel = client.get_channel(payload.channel_id)
@@ -29,12 +45,4 @@ async def on_raw_reaction_add(payload):
     # リアクションを付けたメンバーに役職を付与
     await member.add_roles(role1)
     
-@bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
-    
-@bot.command()
-async def neko(ctx):
-    await ctx.send('にゃ～ん')
-    
-    bot.run(token)
+client.run(token)
